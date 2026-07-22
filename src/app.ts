fd42089b0c9529e769f7c092aad635ffb5c75ab3
Asma <asmaamir64a@gmail.com>
2026-07-22 17:27:14 +0500

    Fix Helmet import for Vercel build

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import path from "node:path";
 import cors from "cors";
-import helmet from "helmet";
+import * as helmet from "helmet";
 import morgan from "morgan";
 import { env } from "./config/env.js";
 import { routes } from "./routes/index.js";
 import { errorHandler, notFound } from "./middleware/error.js";
 
 export const app = express();
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet.default({ contentSecurityPolicy: false }));
 app.use(cors({ origin: env.CORS_ORIGIN === "*" ? true : env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "6mb" }));
 app.use(morgan("dev"));
